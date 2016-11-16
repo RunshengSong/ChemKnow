f3af01c8488ce44305961d6e24bcc25f5c647a41
Runsheng Song <srs90324@gmail.com>
2016-11-15 16:27:25 -0800

    sentence tokenization
    added the sentence tokizenation part, ready for feature embeding.

---
+++ ChemNameIdentifier.py
@@ -0,0 +1,70 @@
+'''
+Created on Nov 15, 2016
+
+@author: rsong_admin
+'''
+import urllib2, time, sys, getopt, json
+
+
+
+PubTator_username = ''
+
+
+def identifyChem(text):
+    trigger = "tmChem"
+
+    #Submit
+
+    url_Submit = "https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/" + trigger + "/Submit/"
+
+    InputSTR = '{"sourcedb":"PubMed","sourceid":"25421723","text":"' + text + '"}'
+
+    urllib_submit = urllib2.urlopen(url_Submit, InputSTR)
+    urllib_result = urllib2.urlopen(url_Submit, InputSTR)
+    SessionNumber = urllib_submit.read()
+
+    if PubTator_username != '':
+        print "Thanks for your submission (Session number: " + SessionNumber + ").\nThe result will be sent to your E-mail: " + email + ".\n"
+    else:
+        print "Thanks for your submission. The session number is : "+ SessionNumber
+        print "The request is received and processing....\n"
+        #Receive
+        url_Receive = "https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/" + SessionNumber + "/Receive/"
+        
+        code=404
+        while(code == 404 or code == 501):
+            time.sleep(5)
+            try:
+                urllib_result = urllib2.urlopen(url_Receive)
+            except urllib2.HTTPError as e:
+                code = e.code
+            except urllib2.URLError as e:
+                code = e.code
+            else:
+                code = urllib_result.getcode()
+
+        # print urllib_result.read()
+        responseJson = urllib_result.read()
+        responseJson = json.loads(responseJson)
+        return parseRespJson(responseJson, text)
+
+
+def parseRespJson(respJson, text):
+    denotations = respJson["denotations"]
+    denotatedChemicals = []
+    for denotation in denotations:
+        chemicalName = text[int(denotation["span"]["begin"]):int(denotation["span"]["end"])]
+        chemicalObj = denotation["obj"]
+        chemicalType = chemicalObj[:chemicalObj.find(":")]
+        chemicalID = chemicalObj[chemicalObj.find(":")+1:]
+        denotatedChemicals.append([chemicalName, chemicalType, chemicalID])
+    return denotatedChemicals
+
+
+
+
+if __name__ == '__main__':
+    text = "as mentioned below, formic acid readily decomposes with concentrated sulfuric acid to form carbon monoxide."
+    identifiedChem = identifyChem(text)
+    for chem in identifiedChem:
+        print chem[0]
--- sentence_trimming.py
+++ sentence_trimming.py
@@ -4,61 +4,125 @@ preprocessing of the positive and negative sentences
 
 @author: rsong_admin
 '''
+from nltk.corpus import stopwords
 
+import csv
+import re
 import pandas as pd
 import numpy as np
 import nltk
 import string
 
 
+def _remove_plural(sentence, product, reactant):
+    '''
+    use lancaster stemmer to remove tense and plural from a sentece
+    '''
+    st = nltk.PorterStemmer()
+    sentence = sentence.split()
+    sentence = " ".join(st.stem(word) for word in sentence)
+    print sentence
+    
+def _replace_chem_name(sentence, product, reactant):
+    '''
+    function to handle the case when product or reactant is in one of the other
+    e.g. product: sodium; reactant: sodium hydroxide
+    
+    also need to deal with the pluarl form of chemical names 
+    '''
+    
+    product = product.strip()
+    reactant = reactant.strip()
+    
+    # remove plural here
+    
+    if product not in reactant and reactant not in product:
+        # if products and reactants are exclusive
+        sentence = re.sub(product+'s?', 'CHEMICAL1', sentence)
+        sentence = re.sub(reactant+'s?','CHEMICAL2', sentence)
+    elif product in reactant:
+        # order matters now
+        sentence = re.sub(reactant+'s?', 'CHEMICAL1', sentence)
+        sentence = re.sub(product+'s?','CHEMICAL2', sentence)
+    elif reactant in product:
+        # order matters 
+        sentence = re.sub(product+'s?', 'CHEMICAL1', sentence)
+        sentence = re.sub(reactant+'s?','CHEMICAL2', sentence)
+
+    return sentence
+
 def _tokenize_sentence(input_sentence, buffer=5):
     '''
     tokenize the sentence, with input buffer
     remove stop words and digits
+    
+    Also stem the tokenized sentence
     '''
+
     input_sentence = " ".join("".join([" " if ch in string.punctuation else ch for ch in input_sentence]).split()) # remove punctuation
     
     token_sentence = nltk.word_tokenize(input_sentence) # tokenize the sentence
+     
+    token_sentence = map(str, token_sentence) # convert each element to string
     
+    ''' remove the stop words here '''
+    token_sentence = [word for word in token_sentence if word not in stopwords.words('english')]
+    
+    ''' remove digits '''
+    token_sentence = [word for word in token_sentence if not word.isdigit()]
+    
+    ''' trimming '''
     first_index = max(0, min(token_sentence.index("CHEMICAL1") - buffer, token_sentence.index("CHEMICAL2") - buffer)) # take the index of the first appearance of the product or reactant, whichever come first, minus the buffer
     last_index = min(len(token_sentence), max(token_sentence.index("CHEMICAL2") + buffer, token_sentence.index("CHEMICAL1") + buffer)) # the same logic as the one above
     
-    output_sentence = token_sentence[first_index: last_index] # trimming here
+    token_sentence = token_sentence[first_index: last_index] # trimming here
     
-
+    return token_sentence
+    
 def trim_sentence(df, buffer=5):
     '''
     this function trim the sentence to only get the words between each pair
     plus few words before and after each pair (depends on the buffer)
     '''
+    trimmed_sentence = []
     for eachRow in df.iterrows():
-        this_product = eachRow[1][0]
-        this_reactant = eachRow[1][1]
+        this_product = eachRow[1][0].strip()
+        this_reactant = eachRow[1][1].strip()
         all_sentences = eachRow[1][2:]
         for eachSentence in all_sentences:
             if not pd.isnull(eachSentence): # check if a cell in pandas is not nan
                 eachSentence = eachSentence.encode('ascii','ignore')
-                
-                eachSentence = eachSentence.replace(this_product,'CHEMICAL1') # replace the actual chemical name with chemical 1 and chemical 2
-                eachSentence = eachSentence.replace(this_reactant,'CHEMICAL2') 
-                
-                ''' still need to replace other chemical names by CHEMICALOTHER '''
-                
-                _tokenize_sentence(eachSentence, buffer=5)
-                
-                raw_input()
-    
-    
 
-
+                # check if each sentence contains both the reactants and products
+                if this_product in eachSentence and this_reactant in eachSentence:
+                    
+                    eachSentence = _replace_chem_name(eachSentence, this_product, this_reactant)
+                    
+                    eachSentence = _tokenize_sentence(eachSentence, buffer=5)
+                    trimmed_sentence.append(eachSentence)
+                else:
+                    # if the sentence does not have both product and reactants
+                    continue
+            else:
+                # break for null sentence, indicating the end of this line
+                break
+            
+    return trimmed_sentence
 
 if __name__ == '__main__':
-#     df = pd.read_excel('./data/positive_cleanup.xlsx',header=None)
-#     trim_sentence(df)
-    from nltk.corpus import stopwords
-    cachedStopWords = stopwords.words("english")
+    df = pd.read_excel('./data/positive_cleanup.xlsx',header=None)
+    trimmed_sentence = trim_sentence(df)
     
-    print cachedStopWords
+    with open('trimmed_sentence.csv','wb') as myfile:
+        thisWriter = csv.writer(myfile)
+        for eachSentence in trimmed_sentence:
+
+            thisWriter.writerow(eachSentence)
+        
+   
+
+    
+
     
     
     
