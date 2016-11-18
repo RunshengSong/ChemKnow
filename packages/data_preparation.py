f3d39331f3e0d50c1bccd5789a3288c7fb592b4a
Runsheng Song <srs90324@gmail.com>
2016-11-18 15:59:24 -0800

    finished the negative data collection from wiki

--- packages/data_preparation.py
+++ packages/data_preparation.py
@@ -12,54 +12,7 @@ import pandas as pd
 import numpy as np
 import nltk
 import string
-
-def _remove_equation(sentence):
-    '''
-    remove the chemical reactaion equation from the sentence
-    '''
-    sentence_list = sentence.splitlines() # split the sentence by lines
-    
-    
-    # remove the title line from the sentence
-    for eachLine in sentence_list:
-        if eachLine.startswith('==') and eachLine.endswith('=='):
-            sentence_list.remove(eachLine)
-    
-    # remove the chemical reaction equation from the sentence
-    for eachLine in sentence_list:
-        if '???' in eachLine and '+' in eachLine:
-            sentence_list.remove(eachLine)
-        
-    return ' '.join(sentence_list)
-
-def clean_up_sentence(input_file, output_file):
-    '''
-    clean up the equations in eachsentence in the input files
-    write the results in the output_file
-    '''
-    with open(input_file,'rb') as myReadfile:
-        with open(output_file,'wb') as myWriterfile:
-            
-            thisReader = csv.reader(myReadfile)
-            thisWriter = csv.writer(myWriterfile)
-            count = 0
-            for eachRow in thisReader:
-                thisResults = []
-                count += 1
-                if count >= 2: # skip the first line
-                    thisProd = eachRow[0]
-                    thisReact = eachRow[1]
-                    thisResults.append(thisProd)
-                    thisResults.append(thisReact)
-                    
-                    allSentence = eachRow[2:] # only dealing with the first sentence for now
-                    for thisSentence in allSentence:
-                        if thisSentence:
-                            thisSentence = _remove_equation(thisSentence) # remove equation and title line if exist
-                            thisResults.append(thisSentence)
-                        else:
-                            break
-                thisWriter.writerow(thisResults)
+from nltk.corpus import wordnet
 
 def _remove_plural(sentence, product, reactant):
     '''
@@ -69,8 +22,15 @@ def _remove_plural(sentence, product, reactant):
     sentence = sentence.split()
     sentence = " ".join(st.stem(word) for word in sentence)
     print sentence
-    
-def _replace_chem_name(sentence, product, reactant):
+
+def _has_number(a_word):
+    '''
+    check if a word has a number in it,
+    if so this might be a chemical formula
+    '''
+    return bool(re.search(r'\d', a_word))
+   
+def _replace_chem_name(sentence, product, reactant=None):
     '''
     function to handle the case when product or reactant is in one of the other
     e.g. product: sodium; reactant: sodium hydroxide
@@ -78,25 +38,58 @@ def _replace_chem_name(sentence, product, reactant):
     also need to deal with the pluarl form of chemical names 
     '''
     
-    product = product.strip()
-    reactant = reactant.strip()
+#     product = product.strip()
+#     reactant = reactant.strip()
+#     
+#     # deal with negative samples, only replace products
+#     if reactant is None:
+#         sentence = re.sub(product+'s?', 'chem', sentence)
+#         
+#     elif product not in reactant and reactant not in product:
+#         # if products and reactants are exclusive
+#         sentence = re.sub(product+'s?', 'chem', sentence)
+#         sentence = re.sub(reactant+'s?','chem', sentence)
+#     elif product in reactant:
+#         # order matters now
+#         sentence = re.sub(reactant+'s?', 'chem', sentence)
+#         sentence = re.sub(product+'s?','chem', sentence)
+#     elif reactant in product:
+#         # order matters 
+#         sentence = re.sub(product+'s?', 'chem', sentence)
+#         sentence = re.sub(reactant+'s?','chem', sentence)
+# 
+#     return sentence
     
-    # remove plural here
-    
-    if product not in reactant and reactant not in product:
-        # if products and reactants are exclusive
-        sentence = re.sub(product+'s?', 'chem', sentence)
-        sentence = re.sub(reactant+'s?','chem', sentence)
-    elif product in reactant:
-        # order matters now
-        sentence = re.sub(reactant+'s?', 'chem', sentence)
-        sentence = re.sub(product+'s?','chem', sentence)
-    elif reactant in product:
-        # order matters 
-        sentence = re.sub(product+'s?', 'chem', sentence)
-        sentence = re.sub(reactant+'s?','chem', sentence)
-
+    # stop using this function for data generation
+    # use Yiting's API to deal with all positive and negative
     return sentence
+
+def _clean_up_wiki_sentence(input_sentence):
+    '''
+    FUNCTION for WIKI
+    
+    clean up the equations in each sentence in the input files
+    write the results in the output_file
+    
+    1. remove equations by lines
+    2. remove title
+    3. 
+    
+    '''
+    sentence_list = input_sentence.splitlines() # split the sentence by lines
+
+    for eachLine in sentence_list:
+        # remove the title line from the sentence
+        if eachLine.startswith('==') and eachLine.endswith('=='):
+            sentence_list.remove(eachLine)
+        # remove the chemical reaction equation from the sentence
+        if '???' in eachLine and '+' in eachLine:
+            sentence_list.remove(eachLine)
+        
+    return ' '.join(sentence_list)  
+
+def _clean_up_sentence(input_sentence):
+    pass
 
 def _trimming(token_sentence, buffer=5):
     '''
@@ -120,7 +113,6 @@ def _trimming(token_sentence, buffer=5):
     token_sentence = token_sentence[first_idx: last_idx] # trimming here
     return token_sentence
         
-
 def _tokenize_sentence(input_sentence, buffer=5):
     '''
     tokenize the sentence, with input buffer
@@ -129,27 +121,37 @@ def _tokenize_sentence(input_sentence, buffer=5):
     Also stem the tokenized sentence
     '''
 
-    input_sentence = " ".join("".join([" " if ch in string.punctuation else ch for ch in input_sentence]).split()) # remove punctuation
+    # remove punctunation
+    input_sentence = " ".join("".join([" " if ch in string.punctuation else ch for ch in input_sentence]).split()) 
     
     token_sentence = nltk.word_tokenize(input_sentence) # tokenize the sentence
-     
-    token_sentence = map(str, token_sentence) # convert each element to string
     
-    ''' remove the stop words here '''
+    # convert each element to string
+    token_sentence = map(str, token_sentence) 
+    
+    
+    # remove stop words
     token_sentence = [word for word in token_sentence if word not in stopwords.words('english')]
     
-    ''' remove digits '''
-    token_sentence = [word for word in token_sentence if not word.isdigit()]
-    
-    ''' trimming '''
-    token_sentence = _trimming(token_sentence,buffer = buffer)
+    # remove digits
+    token_sentence = [word for word in token_sentence if not _has_number(word)]
+               
+    # remove len < 3
+    token_sentence = [word for word in token_sentence if not len(word)<3]
+
+        # remove chemical formula here
+
+    # trim sentence, get only the part between the first and the last occurance of 'chem'
+#     token_sentence = _trimming(token_sentence,buffer = buffer)
         
     return token_sentence
     
-def trim_sentence(df, buffer=10):
+def trim_positive_sentence(df, buffer=10):
     '''
     this function trim the sentence to only get the words between each pair
     plus few words before and after each pair (depends on the buffer)
+    
+    return a list of trimmed sentence regardless the product and reatancts
     '''
     trimmed_sentence = []
     for eachRow in df.iterrows():
@@ -176,7 +178,53 @@ def trim_sentence(df, buffer=10):
                 break
     return trimmed_sentence
 
+def trim_negative_sentence(df, buffer=10):
+    '''
+    this function trim the sentence to only get the words between each pair
+    plus few words before and after each pair (depends on the buffer)
+    
+    return a list of chemicals
+    '''
+    trimmed_sentence = []
+    count = 0
+    for eachRow in df.iterrows():
+        count +=1
+        this_product = eachRow[1][0].strip()
+        this_reactant = eachRow[1][1].strip()
+        all_sentences = eachRow[1][2:]
+        print count
+        
+        for eachSentence in all_sentences:
+            if not pd.isnull(eachSentence): # check if a cell in pandas is not nan
 
+#                 eachSentence = eachSentence.encode('ascii','ignore')
+
+                # check if each sentence contains both the reactants and products
+                if this_product in eachSentence:
+                    eachSentence = _replace_chem_name(eachSentence, this_product)
+
+                    eachSentence = _tokenize_sentence(eachSentence, buffer=buffer)
+                    eachSentence = ' '.join(eachSentence)
+                    trimmed_sentence.append(eachSentence)
+                else:
+                    # if the sentence does not have both product and reactants
+                    continue
+            else:
+                # break for null sentence, indicating the end of this line
+                break
+    return trimmed_sentence
+
+
+if __name__ == '__main__':
+    # test
+    df = pd.read_csv('../data/raw_data/negative_sentence_wiki.csv',header=0)
+    trimmed_negative = trim_negative_sentence(df)
+    
+    with open('negative_sentence_trimmed.csv','wb') as myfile:
+        thisWriter = csv.writer(myfile)
+        for eachSen in trimmed_negative:
+            thisWriter.writerow([eachSen])
+    
 
     
 
--- packages/get_data_from_wiki.py
+++ packages/get_data_from_wiki.py
@@ -12,24 +12,37 @@ import csv
 import re
 import wikipedia as wiki
 from textblob import TextBlob
+from random import shuffle
 
-def get_sentence_for_chem_in_list(list_excel, output_name):
+import data_preparation as dp
+
+
+important_words = ['produced',
+                   'manufactured',
+                   'derivative',
+                   'industrially']
+
+
+def get_positive_sentence_for_chem_in_list(list_excel, output_file_name):
     '''
     This function get data for the chemical pair in the list
     Search sentence from wikipedia
+    
+    This is the function for the positive sentences
     '''
     
     df = pd.ExcelFile(list_excel,header=0).parse('Sheet1')
     
     count = 0
-    
-    with open(output_name,'wb') as myfile:
+    this_results_positive = []
+
+    with open(output_file_name,'wb') as myfile:
         thisWriter = csv.writer(myfile)
         for eachRows in df.iterrows():
             count += 1
             this_product = eachRows[1]['Product'].lower()
             this_rect = eachRows[1]['Reactant'].lower()
-            this_results = []
+            
             
             try:
                 this_page = wiki.WikipediaPage(this_product)
@@ -38,24 +51,83 @@ def get_sentence_for_chem_in_list(list_excel, output_name):
                 zen = TextBlob(raw_text)
                 for eachSentence in zen.sentences:
                     thisSentence = str(eachSentence).lower()
-                    find_product = findWholeWord(this_product)(thisSentence)
-                    find_reactant = findWholeWord(this_rect)(thisSentence)
+                    find_product = _findWholeWord(this_product)(thisSentence)
+                    find_reactant = _findWholeWord(this_rect)(thisSentence)
                     
+                    # if both products and reactant show up in the sentence
+                    # this will be a positive sentence
                     if find_product and find_reactant:
-                        this_results.append(thisSentence)
-                        print 'Sample found for:', this_product,'...', count
+                        this_results_positive.append(thisSentence)
+                        print 'Positive Sample found for:', this_product,'...', count
      
             except (wiki.exceptions.PageError, wiki.exceptions.DisambiguationError):
                 continue
 
-            # write into file
-            thisWriter.writerow([this_product, this_rect]+this_results)
+            # write into file, positive
+            thisWriter.writerow([this_product, this_rect]+this_results_positive)
 
-def findWholeWord(w):
+def get_negative_sentence_for_chem_in_list(list_excel, output_file_name):     
+    '''
+    get negative sentence from wikipedia
+    Rule: the chemical list show up in the sentence but the reactant must not showen up
+    '''
+    df = pd.ExcelFile(list_excel,header=0).parse('Sheet1')   
+    count = 0
+    this_results_negative = []   
+    
+    with open(output_file_name,'wb') as myfile:
+        thisWriter = csv.writer(myfile)   
+        for eachRows in df.iterrows():
+            # for each chemical:
+            
+            count += 1
+            this_product = eachRows[1]['Product'].lower()
+            this_rect = eachRows[1]['Reactant'].lower() 
+            try:
+                this_page = wiki.WikipediaPage(this_product)
+                raw_text = this_page.content
+                
+                zen = TextBlob(raw_text)
+                for eachSentence in zen.sentences:
+                    
+                    thisSentence = str(eachSentence).lower()
+                    thisSentence = dp._clean_up_wiki_sentence(thisSentence)
+                    
+                    find_product = _findWholeWord(this_product)(thisSentence)
+                    find_reactant = _findWholeWord(this_rect)(thisSentence)
+                    find_important_word = _check_important_word(thisSentence)
+                  
+                    # if the product name in the sentence, reactant name is not in the list
+                    # and the imporatn word is not in the list
+                    if find_product and not find_reactant and not find_important_word:
+                        
+                        print 'Negative Sample found for:', this_product,'...', count
+                        this_results_negative.append(thisSentence)
+                
+                # shuffle the elements in the sentence
+                # get rid of the bias of the structure of wiki
+                shuffle(this_results_negative)
+                
+                # only get the first 10 sentences
+                this_results_negative = this_results_negative[0:5]
+                
+            except (wiki.exceptions.PageError, wiki.exceptions.DisambiguationError):
+                continue
+            
+            thisWriter.writerow([this_product, this_rect]+this_results_negative)
+            
+def _check_important_word(sentence):
+    sentence = sentence.split()
+    return any(word for word in important_words if word in sentence)
+            
+def _findWholeWord(w):
+    '''
+    if w in the sentence following this function
+    '''
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
             
-def search_wiki_match_name(product_name, reactant_name):
+def _search_wiki_match_name(product_name, reactant_name):
     '''
     experiment
     '''
@@ -68,12 +140,13 @@ def search_wiki_match_name(product_name, reactant_name):
     zen = TextBlob(raw_text)
     for eachSentence in zen.sentences:
         thisSentence = str(eachSentence).lower()
-        find_or_not = findWholeWord(reactant_name)(thisSentence)
+        find_or_not = _findWholeWord(reactant_name)(thisSentence)
         if find_or_not:
             print thisSentence
             raw_input()
         else:
             print 'No'
+
 
 def search_wiki(chemical_name):
     '''
@@ -89,14 +162,12 @@ def search_wiki(chemical_name):
             
             
 if __name__ == '__main__':
-    list_excel = 'chemical_list.xlsx'
-    output_name = 'positive_sentence_wiki.csv'
+    # test
     
-#     get_sentence_for_chem_in_list(list_excel, output_name)
-#     search_wiki_match_name('trichloroethylene', '1,1,2,2-tetrachloroethane')
-    search_wiki('2-butanol')
-           
-                
+    list_excel = '../data/chemical_list.xlsx'
+    output_name = '../data/raw_data/negative_sentence_wiki.csv'
+    
+    get_negative_sentence_for_chem_in_list(list_excel, output_name)            
         
         
     
