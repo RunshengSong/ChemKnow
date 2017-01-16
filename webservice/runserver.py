4de91c7581c8e33a7026bfd545d151b49a5cfa51
Yiting Ju <yiting.ju@gmail.com>
2017-01-16 14:24:55 -0800

    change port and other functions

--- webservice/runserver.py
+++ webservice/runserver.py
@@ -5,17 +5,72 @@ from flask_cors import CORS, cross_origin
 from annotateChem import annotateChemCS
 from txt2ChemReaction import run_predict_on_text
 from subprocess import Popen
+from datetime import timedelta
+from functools import update_wrapper
 
 
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
+
 
 
 current_dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+				max_age=21600, attach_to_all=True,
+				automatic_options=True):
+
+	if methods is not None:
+		methods = ', '.join(sorted(x.upper() for x in methods))
+	if headers is not None and not isinstance(headers, basestring):
+		headers = ', '.join(x.upper() for x in headers)
+	if not isinstance(origin, basestring):
+		origin = ', '.join(origin)
+	if isinstance(max_age, timedelta):
+		max_age = max_age.total_seconds()
+
+	def get_methods():
+		if methods is not None:
+			return methods
+
+		options_resp = current_app.make_default_options_response()
+		return options_resp.headers['allow']
+
+	def decorator(f):
+		def wrapped_function(*args, **kwargs):
+			if automatic_options and request.method == 'OPTIONS':
+				resp = current_app.make_default_options_response()
+			else:
+				resp = make_response(f(*args, **kwargs))
+			if not attach_to_all and request.method != 'OPTIONS':
+				return resp
+
+			h = resp.headers
+
+			h['Access-Control-Allow-Origin'] = origin
+			h['Access-Control-Allow-Methods'] = get_methods()
+			h['Access-Control-Max-Age'] = str(max_age)
+			if headers is not None:
+				h['Access-Control-Allow-Headers'] = headers
+			return resp
+
+		f.provide_automatic_options = False
+		return update_wrapper(wrapped_function, f)
+	return decorator
+
+
+
+
+
+
+
 @app.route('/annotate', methods=['POST','GET'])
-@cross_origin()
+@crossdomain(origin='*')
+# @cross_origin()
 def run_exposure():
 	if request.method == 'POST':
 		print "post in"
@@ -95,17 +150,25 @@ def run_exposure():
 	elif request.method == "GET":
 		print "get"
 		text = request.args.get('text')
+		targetChem = request.args.get('chem')
+		hideChem = request.args.get('hideChem')
 		print text
 
+		print "111"
+		if str(hideChem) == "1":
+			hideChem = True
+		else:
+			hideChem = False
+		print "Text: ",text
+		print "Target Chemical: ", targetChem
+		print "Hide Chemical Name? ", hideChem
+
 		try:
-			print "111"
-			annotateChem_ins = annotateChem()
-			print "222"
-			apiResponse = annotateChem_ins.search(text)
-			if apiResponse == True:
-				annotateResult = 1
-			else:
-				annotateResult = 0
+			callPopen("python txt2ChemReaction.py " + "\""+ text +"\" " + targetChem + " " + str(hideChem))
+
+			sentList, chemList = readResultFromCSV(os.path.join(current_dir_path, "theResult.csv"))
+
+
 
 		except Exception,e:
 			print str(e)
@@ -116,8 +179,8 @@ def run_exposure():
 			#     'result': {"in":343}
 			#     }})
 			return jsonify({"results": {
-				"annotate": annotateResult,
-				"text": text
+				"sentList": sentList,
+				"chemList": chemList
 				}})
 	else:
 		return jsonify({'error':  {
@@ -128,48 +191,7 @@ def run_exposure():
 
 
 
-def crossdomain(origin=None, methods=None, headers=None,
-				max_age=21600, attach_to_all=True,
-				automatic_options=True):
-	if methods is not None:
-		methods = ', '.join(sorted(x.upper() for x in methods))
-	if headers is not None and not isinstance(headers, basestring):
-		headers = ', '.join(x.upper() for x in headers)
-	if not isinstance(origin, basestring):
-		origin = ', '.join(origin)
-	if isinstance(max_age, timedelta):
-		max_age = max_age.total_seconds()
 
-	def get_methods():
-		if methods is not None:
-			return methods
-
-		options_resp = current_app.make_default_options_response()
-		return options_resp.headers['allow']
-
-	def decorator(f):
-		def wrapped_function(*args, **kwargs):
-			if automatic_options and request.method == 'OPTIONS':
-				resp = current_app.make_default_options_response()
-			else:
-				resp = make_response(f(*args, **kwargs))
-			if not attach_to_all and request.method != 'OPTIONS':
-				return resp
-
-			h = resp.headers
-			h['Access-Control-Allow-Origin'] = origin
-			h['Access-Control-Allow-Methods'] = get_methods()
-			h['Access-Control-Max-Age'] = str(max_age)
-			h['Access-Control-Allow-Credentials'] = 'true'
-			h['Access-Control-Allow-Headers'] = \
-				"Origin, X-Requested-With, Content-Type, Accept, Authorization"
-			if headers is not None:
-				h['Access-Control-Allow-Headers'] = headers
-			return resp
-
-		f.provide_automatic_options = False
-		return update_wrapper(wrapped_function, f)
-	return decorator
 
 
 
@@ -177,7 +199,7 @@ def callPopen(command):
 	print "Calling", command
 	print current_dir_path
 	try:
-	  	e = Popen(
+		e = Popen(
 			command,
 			# cwd="/home/yiting/Downloads/Vega-1.1.1-binaries/vega-cli beta 1_cmd",
 			cwd = current_dir_path,
@@ -212,6 +234,6 @@ def readResultFromCSV(csvFilePath):
 
 
 if __name__ == '__main__':
-	app.run(debug = True, host='0.0.0.0', use_reloader=True, threaded=True)
+	app.run(debug = True, host='0.0.0.0', port=5022, use_reloader=True, threaded=True)
 
 
